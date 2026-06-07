57257ba7d4f0b6cec104005d3f3ee5c31fc645a8
mikkklyubbin <lyubin.misha@mail.ru>
2026-06-07 18:23:32 +0300

    fix dependes

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,2 +1,2 @@
-from initial_transforms import DoubleSizes
-from lenslees_helpers import get_roi
+from src.transforms.initial_transforms import DoubleSizes
+from src.transforms.lenslees_helpers import get_roi
