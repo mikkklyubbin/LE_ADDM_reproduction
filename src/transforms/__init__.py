5f5803dc8c0583a745a146453ba2eab48f128bf3
mik <Lyubin.misha@mail.ru>
2026-06-08 23:51:01 +0300

    add transforms for tensors

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -1,2 +1,3 @@
 from src.transforms.initial_transforms import DoubleSizes
 from src.transforms.lenslees_helpers import get_roi
+from src.transforms.tensor_transforms import DimSwap, Squeeze
---
+++ src/transforms/tensor_transforms.py
@@ -0,0 +1,19 @@
+
+
+import torch
+from torch import nn
+
+class DimSwap(nn.Module):
+    def __init__(self, permutation):
+        super().__init__()
+
+    def forward(self, x):
+        return x.permute(*self.permutation)
+
+class Squeeze(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return x.squeeze(self.dim)
